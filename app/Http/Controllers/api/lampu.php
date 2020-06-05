b01e4dd653628384c82e4d7b7b9aa2a73b95f7c5
gandol <surogento4@gmail.com>
2020-06-05 09:51:28 +0700

    upload

---
+++ app/Http/Controllers/api/lampu.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class lampu extends Controller
+{
+    public function __construct()
+    {
+        date_default_timezone_set('Asia/Jakarta');
+    }
+
+    public function status_lampu($id_user)
+    {
+        try {
+            $dataLampu  = DB::table('lamp_status')->where([
+                'userId' => $id_user
+            ])->value('status');
+            if ($dataLampu) {
+                $data       = [
+                    'status' => 'ok',
+                    'lampu' => 'on'
+                ];
+            } else {
+                $data       = [
+                    'status' => 'ok',
+                    'lampu' => 'off'
+                ];
+            }
+            return response()->json($data, 200);
+        } catch (\Throwable $th) {
+            return response()->json($this->gagal(), 500);
+        }
+    }
+
+    public function gagal()
+    {
+        $dataGagal  = [
+            'status' => 'gagal',
+        ];
+        return $dataGagal;
+    }
+}
